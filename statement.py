4c03830570ca820efa82af3345b50b3c712a97c3
chengziwei <chengziwei@mininglamp.com>
2022-03-10 16:37:54 +0800

    enrich intermediate object, replace all playFor in rederPlainText to use the object

--- statement.py
+++ statement.py
@@ -2,37 +2,43 @@ from math import floor
 
 
 def statement(invoice, plays):
+
+	def enrichPerformance(aPerformance):
+		result = aPerformance.copy()
+		result['play'] = playFor(result)
+		return result
+	
+	def playFor(aPerformance):
+		return plays[aPerformance['playID']]
+	
 	statementData = {}
 	statementData['customer'] = invoice['customer']
-	statementData['performances'] = invoice['performances']
+	statementData['performances'] = list(map(enrichPerformance ,invoice['performances']))
 	return renderPlainText(statementData, plays)
 
 def renderPlainText(data, plays):
 	def usd(aNumber):
 		return f'${aNumber/100:.2f}'
 
-	def playFor(aPerformance):
-		return plays[aPerformance['playID']]
-
 	def amountFor(aPerformance):
 		result = 0
-		if playFor(aPerformance)['type'] == 'tragedy':
+		if aPerformance['play']['type'] == 'tragedy':
 			result = 40000
 			if aPerformance['audience'] > 30:
 				result += 1000 * (aPerformance['audience'] - 30)
-		elif playFor(aPerformance)['type'] == 'comedy':
+		elif aPerformance['play']['type'] == 'comedy':
 			result = 30000
 			if aPerformance['audience'] > 20:
 				result += 10000 + 500 * (aPerformance['audience'] - 20)
 			result += 300 * aPerformance['audience']
 		else:
-			raise Exception(f"unknown type: {playFor(aPerformance)['type']}")
+			raise Exception(f"unknown type: {aPerformance['play']['type']}")
 		return result
 
 	def volumeCreditsFor(aPerformance):
 		result = 0
 		result += max(aPerformance['audience'] - 30, 0)
-		if playFor(aPerformance)['type'] == "comedy":
+		if aPerformance['play']['type'] == "comedy":
 			result += floor(aPerformance['audience'] / 5)
 		return result
 	
@@ -50,7 +56,7 @@ def renderPlainText(data, plays):
 
 	result = f"Statement for {data['customer']}\n"
 	for perf in data['performances']:
-		result += f" {playFor(perf)['name']}: {usd(amountFor(perf))} ({perf['audience']} seats)\n"
+		result += f" {perf['play']['name']}: {usd(amountFor(perf))} ({perf['audience']} seats)\n"
 	result += f'Amount owed is {usd(totalAmount())}\n'
 	result += f'You earned {totalVolumeCredits()} credits\n'
 	return result
