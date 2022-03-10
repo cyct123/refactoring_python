1ab3b3cca8809cf6e065bdc44f529a3c341abc3b
chengziwei <chengziwei@mininglamp.com>
2022-03-10 15:05:31 +0800

    change function declaration

--- statement.py
+++ statement.py
@@ -1,21 +1,6 @@
 from math import floor
 import locale
 
-def amountFor(aPerformance, play):
-	result = 0
-	if play['type'] == 'tragedy':
-		result = 40000
-		if aPerformance['audience'] > 30:
-			result += 1000 * (aPerformance['audience'] - 30)
-	elif play['type'] == 'comedy':
-		result = 30000
-		if aPerformance['audience'] > 20:
-			result += 10000 + 500 * (aPerformance['audience'] - 20)
-		result += 300 * aPerformance['audience']
-	else:
-		raise Exception(f"unknown type: {play['type']}")
-	return result
-
 
 def statement(invoice, plays):
 	totalAmount = 0
@@ -25,8 +10,24 @@ def statement(invoice, plays):
 
 	def playFor(aPerformance):
 		return plays[aPerformance['playID']]
+
+	def amountFor(aPerformance):
+		result = 0
+		if playFor(aPerformance)['type'] == 'tragedy':
+			result = 40000
+			if aPerformance['audience'] > 30:
+				result += 1000 * (aPerformance['audience'] - 30)
+		elif playFor(aPerformance)['type'] == 'comedy':
+			result = 30000
+			if aPerformance['audience'] > 20:
+				result += 10000 + 500 * (aPerformance['audience'] - 20)
+			result += 300 * aPerformance['audience']
+		else:
+			raise Exception(f"unknown type: {playFor(aPerformance)['type']}")
+		return result
+
 	for perf in invoice['performances']:
-		thisAmount = amountFor(perf, playFor(perf))
+		thisAmount = amountFor(perf)
 		
 		volumeCredits += max(perf['audience'] - 30, 0)
 		if playFor(perf)['type'] == "comedy":
