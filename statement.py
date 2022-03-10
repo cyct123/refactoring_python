23c61ae7cbf12ef8a712b995f33f8d01430b9a2a
chengziwei <chengziwei@mininglamp.com>
2022-03-10 14:50:21 +0800

    refact:extract function amountFor

--- statement.py
+++ statement.py
@@ -1,33 +1,37 @@
 from math import floor
 import locale
 
+def amountFor(perf, play):
+	thisAmount = 0
+	if play['type'] == 'tragedy':
+		thisAmount = 40000
+		if perf['audience'] > 30:
+			thisAmount += 1000 * (perf['audience'] - 30)
+	elif play['type'] == 'comedy':
+		thisAmount = 30000
+		if perf['audience'] > 20:
+			thisAmount += 10000 + 500 * (perf['audience'] - 20)
+		thisAmount += 300 * perf['audience']
+	else:
+		raise Exception(f"unknown type: {play['type']}")
+	return thisAmount
+
 
 def statement(invoice, plays):
 	totalAmount = 0
 	volumeCredits = 0
-	result = "Statement for {}\n".format(invoice['customer'])
+	result = f"Statement for {invoice['customer']}\n"
 	locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 	for perf in invoice['performances']:
 		play = plays[perf['playID']]
-		thisAmount = 0
-		if play['type'] == 'tragedy':
-			thisAmount = 40000
-			if perf['audience'] > 30:
-				thisAmount += 1000 * (perf['audience'] - 30)
-		elif play['type'] == 'comedy':
-			thisAmount = 30000
-			if perf['audience'] > 20:
-				thisAmount += 10000 + 500 * (perf['audience'] - 20)
-			thisAmount += 300 * perf['audience']
-		else:
-			raise Exception("unknown type: {}".format(play['type']))
+		thisAmount = amountFor(perf, play)
 		
 		volumeCredits += max(perf['audience'] - 30, 0)
 		if play['type'] == "comedy":
 			volumeCredits += floor(perf['audience'] / 5)
 
-		result += " {}: {} ({} seats)\n".format(play['name'], locale.currency(thisAmount / 100), perf['audience'])
+		result += f" {play['name']}: {locale.currency(thisAmount / 100)} ({perf['audience']} seats)\n"
 		totalAmount += thisAmount
-	result += 'Amount owed is {}\n'.format(locale.currency(totalAmount / 100))
-	result += 'You earned {} credits\n'.format(volumeCredits)
+	result += f'Amount owed is {locale.currency(totalAmount / 100)}\n'
+	result += f'You earned {volumeCredits} credits\n'
 	return result
