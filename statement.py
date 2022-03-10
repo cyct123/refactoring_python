7cc0cafea4e32a0acb58b47af996a8d659f8c4b4
chengziwei <chengziwei@mininglamp.com>
2022-03-10 16:30:01 +0800

    pass data to imtermediate object

--- statement.py
+++ statement.py
@@ -3,9 +3,11 @@ from math import floor
 
 def statement(invoice, plays):
 	statementData = {}
-	return renderPlainText(statementData, invoice, plays)
+	statementData['customer'] = invoice['customer']
+	statementData['performances'] = invoice['performances']
+	return renderPlainText(statementData, plays)
 
-def renderPlainText(data, invoice, plays):
+def renderPlainText(data, plays):
 	def usd(aNumber):
 		return f'${aNumber/100:.2f}'
 
@@ -36,18 +38,18 @@ def renderPlainText(data, invoice, plays):
 	
 	def totalVolumeCredits():
 		result = 0
-		for perf in invoice['performances']:
+		for perf in data['performances']:
 			result += volumeCreditsFor(perf)
 		return result
 	
 	def totalAmount():
 		result = 0
-		for perf in invoice['performances']:
+		for perf in data['performances']:
 			result += amountFor(perf)
 		return result
 
-	result = f"Statement for {invoice['customer']}\n"
-	for perf in invoice['performances']:
+	result = f"Statement for {data['customer']}\n"
+	for perf in data['performances']:
 		result += f" {playFor(perf)['name']}: {usd(amountFor(perf))} ({perf['audience']} seats)\n"
 	result += f'Amount owed is {usd(totalAmount())}\n'
 	result += f'You earned {totalVolumeCredits()} credits\n'
