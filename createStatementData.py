491c482986da6e5818e37196abd1e3d138728296
chengziwei <chengziwei@mininglamp.com>
2022-03-10 18:21:50 +0800

    replace conditional with polymorphism

--- createStatementData.py
+++ createStatementData.py
@@ -10,32 +10,47 @@ class PerformanceCalculator:
 
 	@property
 	def amount(self):
-		result = 0
-		if self.play['type'] == 'tragedy':
-			result = 40000
-			if self.performance['audience'] > 30:
-				result += 1000 * (self.performance['audience'] - 30)
-		elif self.play['type'] == 'comedy':
-			result = 30000
-			if self.performance['audience'] > 20:
-				result += 10000 + 500 * (self.performance['audience'] - 20)
-			result += 300 * self.performance['audience']
-		else:
-			raise Exception(f"unknown type: {self.play['type']}")
+		raise NotImplementedError("subclass responsibility")
+
+	@property
+	def volumeCredits(self):
+		return max(self.performance['audience'] - 30, 0)
+
+class TragedyCalculator(PerformanceCalculator):
+
+	@property
+	def amount(self):
+		result = 40000
+		if self.performance['audience'] > 30:
+			result += 1000 * (self.performance['audience'] - 30)
+		return result
+
+class ComedyCalculator(PerformanceCalculator):
+
+	@property
+	def amount(self):
+		result = 30000
+		if self.performance['audience'] > 20:
+			result += 10000 + 500 * (self.performance['audience'] - 20)
+		result += 300 * self.performance['audience']
 		return result
 
 	@property
 	def volumeCredits(self):
-		result = 0
-		result += max(self.performance['audience'] - 30, 0)
-		if self.play['type'] == "comedy":
-			result += floor(self.performance['audience'] / 5)
-		return result
+		return super().volumeCredits + floor(self.performance['audience'] / 5)
+
+def createPerformancCalculator(aPerformance, aPlay):
+	if aPlay['type'] == 'tragedy':
+		return TragedyCalculator(aPerformance, aPlay)
+	elif aPlay['type'] == 'comedy':
+		return ComedyCalculator(aPerformance, aPlay)
+	else:
+		raise Exception(f"unknown type: {aPlay['type']}")
 
 def createStatementData(invoice, plays):
 
 	def enrichPerformance(aPerformance):
-		calculator = PerformanceCalculator(aPerformance, playFor(aPerformance))
+		calculator = createPerformancCalculator(aPerformance, playFor(aPerformance))
 		result = aPerformance.copy()
 		result['play'] = calculator.play
 		result['amount'] = calculator.amount
